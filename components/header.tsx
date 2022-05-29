47ca3757b34f4a4cfd13e1f65767ff0313726238
Sonokui <t.sonokui@la-haina.jp>
2022-05-29 09:41:43 +0900

    prep for more customization

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,6 @@ const Header = () => {
       <Link href='/'>
         <a className='hover:underline'>No Hedge</a>
       </Link>
-      .
     </h2>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,12 @@
-import { CMS_NAME } from '../lib/constants'
+import Link from 'next/link'
 
 const Intro = () => {
   return (
     <section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
       <h1 className='text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'>
-        No Hedge.
+        <Link href='/'>No Hedge</Link>
       </h1>
+      {/* ここにナビゲーションバーを埋め込む */}
     </section>
   )
 }
