aea12a20b8c18297ac8dc51e4d825c53f836342f
Sonokui <t.sonokui@la-haina.jp>
2022-06-11 17:41:59 +0900

    a bit more CLEANUPs

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,4 +1,3 @@
-import Avatar from './avatar'
 import DateFormatter from './date-formatter'
 import CoverImage from './cover-image'
 import Link from 'next/link'
@@ -13,14 +12,7 @@ type Props = {
   slug: string
 }
 
-const PostPreview = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) => {
+const PostPreview = ({ title, coverImage, date, excerpt, slug }: Props) => {
   return (
     <div>
       <div className='mb-5'>
