2ef041a69798d66c44f4bc25af19245ba1eaf9fe
Sonokui <wlcmty08kh@gmail.com>
2022-07-25 23:10:41 +0900

    Fix/ footer position

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,11 @@ import Container from './container'
 const Footer = () => {
   return (
     <footer>
-      <Container>&copy; 2022 No Hedge. All rights reserved.</Container>
+      <Container>
+        <p className='mt-8 pb-10 text-center text-white'>
+          &copy; 2022 No Hedge. All rights reserved.
+        </p>
+      </Container>
     </footer>
   )
 }
