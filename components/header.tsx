4d05eba2988fec0daf51ab3ea7ee8cd4e2b85be6
Sonokui <wlcmty08kh@gmail.com>
2022-07-25 22:20:16 +0900

    Fix/ primary color of text and background

--- components/header.tsx
+++ components/header.tsx
@@ -4,9 +4,7 @@ const Header = () => {
   return (
     <div className='h-16 flex mx-auto max-w-7xl text-2xl md:text-4xl font-bold text-primary-text tracking-tight md:tracking-tighter leading-tight mb-6'>
       <h2 className='flex items-center px-8 md:px-0'>
-        <Link href='/'>
-          <a className='hover:underline'>No Hedge</a>
-        </Link>
+        <Link href='/'>No Hedge</Link>
       </h2>
     </div>
   )
